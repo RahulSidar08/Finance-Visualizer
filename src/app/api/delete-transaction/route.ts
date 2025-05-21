b06bd2d63c5b63f1a439b8f563d264893532c267
RahulSidar08 <rahulsidar2056@gmail.com>
2025-05-21 18:44:30 +0530

    fix

--- src/app/api/delete-transaction/route.ts
+++ src/app/api/delete-transaction/route.ts
@@ -1,32 +1,30 @@
+import { NextRequest, NextResponse } from "next/server";
 import connectDb from "@/lib/connectDb";
 import TransactionModel from "@/model/transactionModel";
-import { NextRequest, NextResponse } from "next/server";
 
-export async function DELETE(
-  request: NextRequest,
-  context: { params: { id: string } }
-) {
+
+export async function DELETE(request: NextRequest) {
   try {
     await connectDb();
 
-    const { id } = context.params;
+    // Extract the 'id' from the URL
+    const url = new URL(request.url);
+    const parts = url.pathname.split("/");
+    const id = parts[parts.length - 1];
 
     const transaction = await TransactionModel.findByIdAndDelete(id);
 
     if (!transaction) {
       return NextResponse.json(
-        {
-          success: false,
-          message: "Transaction not found",
-        },
+        { success: false, message: "Transaction not found" },
         { status: 404 }
       );
     }
 
-    return NextResponse.json({
-      success: true,
-      message: "Transaction deleted successfully",
-    });
+    return NextResponse.json(
+      { success: true, message: "Transaction deleted successfully" },
+      { status: 200 }
+    );
   } catch (error) {
     return NextResponse.json(
       {
@@ -38,3 +36,5 @@ export async function DELETE(
     );
   }
 }
+
+
